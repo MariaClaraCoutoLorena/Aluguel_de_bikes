20a2b11a5cb6658d28b1b8d98cf7c7dd0bd5fef9
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2023-08-30 17:34:53 -0300

    Index test

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Bike } from "./bike";
+import { Bike } from './bike';
 import { Rent } from "./rent";
 import { User } from "./user";
 import crypto from 'crypto';
@@ -20,6 +20,7 @@ export class App {
         }
         user.id = crypto.randomUUID()
         this.users.push(user)
+        console.log("User cadastrado com sucessor!")
     }
 
     registerBike(bike: Bike): void {
@@ -34,29 +35,36 @@ export class App {
             bike.id = crypto.randomUUID() 
         }
         this.bikes.push(bike)
+        console.log("Bike cadastrada com sucessor!")
     }
     removeUser(user: User): void {
         let index = this.users.indexOf(user)
         if(index) this.users.splice(index,1)
         else throw new Error('Usuário não encontrada.')
+        console.log("User retirado do sistema!")
     }
 
     removeBike(bike: Bike): void {
         let index = this.bikes.indexOf(bike)
         if(index) this.bikes.splice(index,1)
         else  throw new Error('Bike não encontrada.')
+        console.log("Bike retirada do sistema!")
     }
 
     rentBike(bike: Bike, user: User ,startDate: Date, endDate: Date): void{
         let rents_bike = this.rents.filter(rent => {rent.bike == bike})
         let new_rent = Rent.create(rents_bike, bike, user, startDate, endDate )
         this.rents.push(new_rent)
+        console.log("Bike reservada! :)")
     }
 
-    returnBike(bike: Bike, user: User ,startDate: Date, endDate: Date): void{
-        let rent_bike = this.rents.find(rent => {rent.bike == bike && rent.dateReturned==undefined})
-        if(rent_bike) rent_bike.dateReturned = new Date()
-        else throw new Error('Essa marca não estava reservada.')
+    returnBike(bike: Bike): void{
+        let rent_bike = this.rents.find((rent) => {rent.bike == bike})
+        if(rent_bike){
+            rent_bike.dateReturned = new Date()
+            console.log("Bike entregue com sucesso!")
+            if( rent_bike.dateReturned>rent_bike.dateTo) console.log("Mas tome cuidado para não atrasar de novo!")
+        } else throw new Error('Essa bike não estava reservada.')
     }
 
 }
