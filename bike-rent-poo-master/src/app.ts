c1f32da3d3e13618e360d565746d90338e5204af
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2023-08-30 14:28:20 -0300

    Código base

---
+++ bike-rent-poo-master/src/app.ts
@@ -0,0 +1,22 @@
+import { Bike } from "./bike";
+import { Rent } from "./rent";
+import { User } from "./user";
+
+export class App {
+    users: User[] = []
+    bikes: Bike[] = []
+    rents: Rent[] = []
+
+    findUser(email: string): User | undefined {
+        return this.users.find(user => { return user.email === email})
+    }
+
+    registerUser(user: User): void {
+        for (const rUser of this.users) {
+            if (rUser.email === user.email) {
+                throw new Error('Duplicate user.')
+            }
+        }
+        this.users.push(user)
+    }
+}
